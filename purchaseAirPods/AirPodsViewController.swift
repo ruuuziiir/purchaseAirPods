3d76742db25c83066540abba86f4802d70d0039f
7 <ruuuziiir@gmail.com>
2021-03-21 01:11:22 +0800

    AirPods comment added

--- purchaseAirPods/AirPodsViewController.swift
+++ purchaseAirPods/AirPodsViewController.swift
@@ -20,29 +20,32 @@ class AirPodsViewController: UIViewController, saveDataDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 設定 input 初始狀態為空字串
         inputLabel.text = ""
         
+        // 設定「加上鐫刻」按鈕的框線以及圓角
         addWordButton.layer.borderColor = UIColor.lightGray.cgColor
         addWordButton.layer.borderWidth = 1
         addWordButton.layer.cornerRadius = 10
         
+        // 設定「加入購物袋」按鈕的圓角
         addCartButton.layer.cornerRadius = 10
         
+    }
 
-    }
-    @IBAction func addLabel(_ sender: Any) {
-        
-    }
     
+    // 任命變數 count，用於計算加入購物車的按鈕被點擊的次數
     var count = 0
 
     @IBAction func addCartNumChange(_ sender: Any) {
         count = count + 1
+        
+        // tabBarController 中最後一個 item 的 badgeValue 改變
         tabBarController?.tabBar.items?.last?.badgeValue = String(count)
 
     }
     
-    // Delegate Method
+    //
       func replaceLabelName(inputLabelName: String) {
           self.inputLabel.text = inputLabelName
       }
